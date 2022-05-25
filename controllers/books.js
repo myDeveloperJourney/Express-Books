a6fde68b3027764ea6759298b31ac8b48384b687
Daniel JS <daniel@mydeveloperjourney.com>
2022-05-25 13:07:37 -0500

    wip: adds controller module

---
+++ controllers/books.js
@@ -0,0 +1,8 @@
+// require dependencies
+const express = require('express'); // node modules use the singleton pattern
+// initialize the router object
+const router = express.Router(); // factory function - function that returns an object once invoked
+// define router/controller code
+
+// export the router object using module.exports
+module.exports = router;
